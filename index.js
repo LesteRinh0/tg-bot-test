2af2ec6d7c6e55392740cdeb8c0838e31a3342af
LesteR <example@gmail.com>
2023-06-14 20:32:04 +0300

    weather

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ bot.on("message", async (msg) => {
   const catImage = "https://meow.senither.com/v1/random";
   const text = msg.text;
   const chatId = msg.chat.id;
-  console.log(msg);
+  console.log(msg.text);
 
   if (text === "/start") {
     bot.sendMessage(chatId, `Добро пожаловать ${msg.chat.first_name}!`);
@@ -39,30 +39,31 @@ bot.on("message", async (msg) => {
 });
 bot.onText(/\/weather/, (msg) => {
   bot.sendMessage(msg.chat.id, "Привет! В каком городе вы находитесь?");
+  if (msg.text != "/weather") {
+    bot.sendMessage(msg.chat.id, "Город не введен.");
+  } else {
+    bot.onText(/(.+)/, async (msg, match) => {
+      const city = match[1];
+
+      try {
+        const result = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API}&units=metric`
+        );
+        const { name, weather, main } = result.data;
+
+        const description = weather[0].description;
+        const temp = main.temp;
+
+        bot.sendMessage(
+          msg.chat.id,
+          `В городе ${name} сейчас ${description}. Температура составляет ${temp}°C.`
+        );
+      } catch (error) {
+        bot.sendMessage(
+          msg.chat.id,
+          "Не удалось найти такой город. Попробуйте еще раз."
+        );
+      }
+    });
+  }
 });
-
-if (msg.text === "/weather") {
-  bot.onText(/(.+)/, async (msg, match) => {
-    const city = match[1];
-
-    try {
-      const result = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API}&units=metric`
-      );
-      const { name, weather, main } = result.data;
-
-      const description = weather[0].description;
-      const temp = main.temp;
-
-      bot.sendMessage(
-        msg.chat.id,
-        `В городе ${name} сейчас ${description}. Температура составляет ${temp}°C.`
-      );
-    } catch (error) {
-      bot.sendMessage(
-        msg.chat.id,
-        "Не удалось найти такой город. Попробуйте еще раз."
-      );
-    }
-  });
-}
