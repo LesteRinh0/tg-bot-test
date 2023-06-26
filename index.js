20cd5556d5aa375fc68a81f75fd2332c18702190
LesteR <example@gmail.com>
2023-06-26 19:40:05 +0300

    444

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ import { everyDayNotify } from "./src/subscribe.js";
 import schedule from "node-schedule";
 import { MongoClient } from "mongodb";
 import TelegramApi from "node-telegram-bot-api";
+var express = require("express");
+var packageInfo = require("./package.json");
+
+var app = express();
+
+app.get("/", function (req, res) {
+  res.json({ version: packageInfo.version });
+});
+
+var server = app.listen(process.env.PORT || 5000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Web server started at http://%s:%s", host, port);
+});
 
 const client = new MongoClient(process.env.URL);
 const bot = new TelegramApi(process.env.TOKEN, { polling: true });
