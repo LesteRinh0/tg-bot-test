19cdb6c08606d5e49d65f106d4d683f184ca2ae5
LesteR <example@gmail.com>
2023-06-28 14:51:34 +0300

    feat: add mainFunctions

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { keys, errors, links } from './src/constants.js';
 import { everyDayNotify } from './src/subscribe.js';
 import { client, collection } from './src/mongoConfig.js';
 import { bot } from './src/botConfig.js';
-import { server } from './src/serverConfig.js';
+import { processCommand } from './src/mainFunctions.js';
 
 client.connect();
 
@@ -33,59 +33,7 @@ bot.onText(/(.+)/, async (msg, match) => {
   const text = match[1];
   const chatId = msg.chat.id;
 
-  if (msg.entities && msg.entities[0].type === 'bot_command') {
-    if (text === '/start') {
-      bot.sendMessage(chatId, `Добро пожаловать ${msg.chat.first_name}!`);
-    }
-    if (text === '/weather') {
-      bot.sendMessage(
-        chatId,
-        'Не введен город при вызове команды! Пример: /weather Минск'
-      );
-    }
-    if (text === '/recommend') {
-      bot.sendMessage(
-        chatId,
-        'Не введен город при вызове команды! Пример: /recommend Минск'
-      );
-    }
-    if (text === '/subscribe') {
-      bot.sendMessage(
-        chatId,
-        'Не введен город при вызове команды! Пример: /subscribe Минск'
-      );
-    }
-    if (text === '/unsubscribe') {
-      bot.sendMessage(
-        chatId,
-        'Не введен город при вызове команды! Пример: /unsubscribe Минск'
-      );
-    }
-
-    if (text === '/cat') {
-      const response = await axios.get(links.cat_url);
-      const cat = response.data.data.url;
-      await bot.sendPhoto(chatId, cat);
-    }
-
-    if (text === '/dog') {
-      const response = await axios.get(links.dog_url);
-      const dog = response.data.message;
-      await bot.sendPhoto(chatId, dog);
-    }
-    if (text === '/help') {
-      bot.sendMessage(
-        chatId,
-        `Стандартные команды представлены в меню!
-Дополнительные команды:
-/subscribe *Город* - подписка на ежедневные уведомления о погоде введенного города;
-/unsubscribe *Город* - отписка от ежедневных уведомлений о погоде введенного города;
-/weather *Город* - сведения о погоде выбранного города;
-/recommend *Город* - рекомендации по местам, которые можно посетить в введенном городе
-/createTask - создание задач и напоминаний`
-      );
-    }
-  }
+  processCommand(text, chatId);
 });
 bot.onText(/\/recommend (.+)/, async (msg, match) => {
   const chatId = msg.chat.id;
---
+++ src/mainFunctions.js
@@ -0,0 +1,44 @@
+export async function processCommand(text, chatId) {
+    if (text === '/start') {
+      bot.sendMessage(chatId, `Добро пожаловать ${msg.chat.first_name}!`);
+    } else if (text === '/weather') {
+      bot.sendMessage(
+        chatId,
+        'Не введен город при вызове команды! Пример: /weather Минск'
+      );
+    } else if (text === '/recommend') {
+      bot.sendMessage(
+        chatId,
+        'Не введен город при вызове команды! Пример: /recommend Минск'
+      );
+    } else if (text === '/subscribe') {
+      bot.sendMessage(
+        chatId,
+        'Не введен город при вызове команды! Пример: /subscribe Минск'
+      );
+    } else if (text === '/unsubscribe') {
+      bot.sendMessage(
+        chatId,
+        'Не введен город при вызове команды! Пример: /unsubscribe Минск'
+      );
+    } else if (text === '/cat') {
+      const response = await axios.get(links.cat_url);
+      const cat = response.data.data.url;
+      await bot.sendPhoto(chatId, cat);
+    } else if (text === '/dog') {
+      const response = await axios.get(links.dog_url);
+      const dog = response.data.message;
+      await bot.sendPhoto(chatId, dog);
+    } else if (text === '/help') {
+      bot.sendMessage(
+        chatId,
+        `Стандартные команды представлены в меню!
+  Дополнительные команды:
+  /subscribe *Город* - подписка на ежедневные уведомления о погоде введенного города;
+  /unsubscribe *Город* - отписка от ежедневных уведомлений о погоде введенного города;
+  /weather *Город* - сведения о погоде выбранного города;
+  /recommend *Город* - рекомендации по местам, которые можно посетить в введенном городе
+  /createTask - создание задач и напоминаний`
+      );
+    }
+  }
