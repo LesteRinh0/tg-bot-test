c3f87e38a96ad7c20f5eb132e6f03d9b411fedac
LesteR <example@gmail.com>
2023-06-28 11:14:25 +0300

    fix: procces.env problem

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@ export const key = {
   port: process.env.PORT,
   url: process.env.URL,
   token: process.env.TOKEN,
-  weather_api: process.nv.WEATHER_API,
+  weather_api: process.env.WEATHER_API,
   place_api: process.env.PLACE_API,
   dog_url: process.env.DOG_URL,
   cat_url: process.env.CAT_URL,
