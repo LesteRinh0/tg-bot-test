59a6449c13f9d49ce6260f3d20f358dab680e331
LesteR <example@gmail.com>
2023-06-28 17:45:16 +0300

    fix: server import

--- src/serverConfig.js
+++ src/serverConfig.js
@@ -1,14 +1,6 @@
 import express from 'express';
 
-import { keys } from './constants.js';
-
-const app = express();
+export const app = express();
 app.get('/', (res) => {
   res.json({ version: '1.0.0' });
-});
-
-export const server = app.listen(keys.port || 5000, () => {
-  const host = server.address().address;
-  const { port } = server.address();
-  console.log('Web server started at http://%s:%s', host, port);
 });
