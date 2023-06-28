1d6512862503424b83573c42c470581b3fbd85cb
LesteR <example@gmail.com>
2023-06-28 11:08:42 +0300

    fix: dotenv -> depend

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,8 +19,7 @@ module.exports = {
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
     'no-console': 0,
-    
-  },
+      },
   overrides: [
     {
       env: {
