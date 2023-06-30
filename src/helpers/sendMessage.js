3f0fb0569aee332a1be9ec7e111b01a739a625e5
LesteR <example@gmail.com>
2023-06-30 11:03:18 +0300

    fix: split mainFunc

---
+++ src/helpers/sendMessage.js
@@ -0,0 +1,28 @@
+import { messageHelp } from "../constants/constants";
+  
+  export async function notifyStartCommand(chatId, bot, firstName) {
+    bot.sendMessage(chatId, `Добро пожаловать ${firstName}!`);
+  }
+  
+  export async function notifyMissingCity(chatId, bot, command) {
+    bot.sendMessage(
+      chatId,
+      `Не введен город при вызове команды! Пример: ${command} Минск`
+    );
+  }
+  
+  export async function sendCatPhoto(chatId, bot, axios, catUrl) {
+    const response = await axios.get(catUrl);
+    const cat = response.data.data.url;
+    await bot.sendPhoto(chatId, cat);
+  }
+  
+  export async function sendDogPhoto(chatId, bot, axios, dogUrl) {
+    const response = await axios.get(dogUrl);
+    const dog = response.data.message;
+    await bot.sendPhoto(chatId, dog);
+  }
+  
+  export async function sendHelpMessage(chatId, message, bot) {
+    bot.sendMessage(chatId, messageHelp);
+}
