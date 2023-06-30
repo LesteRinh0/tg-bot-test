e3549dc0508e84c8d33d449ff4114e5a41c860b4
LesteR <example@gmail.com>
2023-06-30 14:43:37 +0300

    fix tasks

--- src/commands/tasks.js
+++ src/commands/tasks.js
@@ -7,7 +7,7 @@ export async function viewTasks(msg) {
     try {
         const tasks = await collection.find({ id: chatId, task: { $exists: true } }).toArray();
         let tasksMsg = 'Ваши задачи:\n\n';;
-        if (!!tasks[0].task) {
+        if (!tasks[0].task) {
             tasksMsg = 'У вас нету запланированных задач';
         } else {
             tasks.forEach((task) => {
@@ -15,6 +15,6 @@ export async function viewTasks(msg) {
         }       
         bot.sendMessage(chatId, tasksMsg);
     } catch (error) {
-        sendErrorMessage();
+        sendErrorMessage(chatId, bot);
     }
 }
