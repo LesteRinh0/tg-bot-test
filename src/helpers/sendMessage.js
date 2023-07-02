3dcf30834a4386b0df2594191a7cd32d7412080f
LesteR <example@gmail.com>
2023-07-02 18:58:19 +0300

    feat: notify about subs

--- src/helpers/sendMessage.js
+++ src/helpers/sendMessage.js
@@ -1,28 +1,45 @@
-  
-  export async function notifyStartCommand(chatId, bot, firstName) {
+import { collection } from "../configs/mongoConfig.js";
+import { sendErrorMessage } from "./recommendFunc.js";
+
+export async function notifyStartCommand(chatId, bot, firstName) {
     bot.sendMessage(chatId, `Добро пожаловать ${firstName}!`);
   }
   
-  export async function notifyMissingCity(chatId, bot, command) {
+export async function notifyMissingCity(chatId, bot, command) {
     bot.sendMessage(
       chatId,
       `Не введен город при вызове команды! Пример: ${command} Минск`
     );
+    if (command === '/unsubscribe'){
+      try {
+        const cities = await collection.find({ id: chatId, city: { $exists: true } }).toArray();
+        let cityMsg = 'Ваш список подписок на города:\n\n';;
+        if (cities.length === 0) {
+            cityMsg = 'У вас нету активных подписок на города';
+        } else {
+            tasks.forEach((city) => {
+                    cityMsg += `• ${city.city}\n`;});
+        }       
+        bot.sendMessage(chatId, cityMsg);
+    } catch (error) {
+        sendErrorMessage(chatId, bot);
+    }
+    }
   }
 
-  export async function sendCatPhoto(chatId, bot, axios, catUrl) {
+export async function sendCatPhoto(chatId, bot, axios, catUrl) {
     const response = await axios.get(catUrl);
     const cat = response.data.data.url;
     await bot.sendPhoto(chatId, cat);
   }
   
-  export async function sendDogPhoto(chatId, bot, axios, dogUrl) {
+export async function sendDogPhoto(chatId, bot, axios, dogUrl) {
     const response = await axios.get(dogUrl);
     const dog = response.data.message;
     await bot.sendPhoto(chatId, dog);
   }
-  
-  export async function sendHelpMessage(chatId, bot) {
+
+export async function sendHelpMessage(chatId, bot) {
     bot.sendMessage(
       chatId,
       `Стандартные команды представлены в меню!
