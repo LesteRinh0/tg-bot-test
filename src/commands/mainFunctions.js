716077b19f4ec5f25aa775dbf6e9e0a2d680266d
LesteR <example@gmail.com>
2023-07-03 13:29:26 +0300

    fix: named import

--- src/commands/mainFunctions.js
+++ src/commands/mainFunctions.js
@@ -1,32 +1,32 @@
 import axios from 'axios';
 import { links } from '../constants/constants.js';
-import { isStartCommand, isCatCommand, isDogCommand, isHelpCommand, isRecommendCommand, isSubscribeCommand, isUnsubscribeCommand, isWeatherCommand } from '../helpers/helpers.js';
+import * as commands from '../helpers/helpers.js';
 import { notifyStartCommand, notifyMissingCity, sendCatPhoto, sendDogPhoto, sendHelpMessage } from '../helpers/sendMessage.js';
 
 export async function processCommand(text, chatId, bot, msg) {
   switch (true) {
-    case isStartCommand(text):
+    case commands.isStartCommand(text):
       await notifyStartCommand(chatId, bot, msg.chat.first_name);
       break;
-    case isWeatherCommand(text):
+    case commands.isWeatherCommand(text):
       await notifyMissingCity(chatId, bot, "/weather");
       break;
-    case isRecommendCommand(text):
+    case commands.isRecommendCommand(text):
       await notifyMissingCity(chatId, bot, "/recommend");
       break;
-    case isSubscribeCommand(text):
+    case commands.isSubscribeCommand(text):
       await notifyMissingCity(chatId, bot, "/subscribe");
       break;
-    case isUnsubscribeCommand(text):
+    case commands.isUnsubscribeCommand(text):
       await notifyMissingCity(chatId, bot, "/unsubscribe");
       break;
-    case isCatCommand(text):
+    case commands.isCatCommand(text):
       await sendCatPhoto(chatId, bot, axios, links.cat_url);
       break;
-    case isDogCommand(text):
+    case commands.isDogCommand(text):
       await sendDogPhoto(chatId, bot, axios, links.dog_url);
       break;
-    case isHelpCommand(text):
+    case commands.isHelpCommand(text):
       await sendHelpMessage(chatId, bot, text);
       break;
     default:
