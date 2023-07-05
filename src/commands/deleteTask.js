f2008f8c3125dec71a3afee027e5ed829d6e5d4b
LesteR <example@gmail.com>
2023-07-05 17:46:54 +0300

    fix deleteTask func

--- src/commands/deleteTask.js
+++ src/commands/deleteTask.js
@@ -9,29 +9,29 @@ export async function deleteTask(msg) {
         let tasksMsg = 'Ваши задачи:\n\n';;
         if (tasks.length === 0) {
             tasksMsg = 'У вас нету запланированных задач';
+            bot.sendMessage(chatId, tasksMsg);
         } else {
             tasks.forEach((task) => {
                     tasksMsg += `• ${task.task}\n`;});
-        }       
-        
-      bot.sendMessage(chatId, 'Выберите задачу для удаления:\n' + tasksMsg);
+          bot.sendMessage(chatId, 'Выберите задачу для удаления:\n' + tasksMsg);
 
-      bot.once('message', (msg) => {
-        const selectedTaskIndex = parseInt(msg.text) - 1;
+          bot.once('message', (msg) => {
+          const selectedTaskIndex = parseInt(msg.text) - 1;
 
-        if (isNaN(selectedTaskIndex) || selectedTaskIndex < 0 || selectedTaskIndex >= tasks.length) {
+          if (isNaN(selectedTaskIndex) || selectedTaskIndex < 0 || selectedTaskIndex >= tasks.length) {
           bot.sendMessage(chatId, 'Некорректный номер задачи. Попробуйте еще раз');
           return;
-        }
+          }
 
-        const selectedTask = tasks[selectedTaskIndex];
+          const selectedTask = tasks[selectedTaskIndex];
 
-        collection.deleteOne({ _id: selectedTask._id }, (err, result) => {
+          collection.deleteOne({ _id: selectedTask._id }, (err, result) => {
           if (err) {
             sendErrorMessage(chatId, bot);
           }
 
           bot.sendMessage(chatId, `Задача "${selectedTask.name}" успешно удалена`);
-        });
-      });
+            });
+          });
+        }            
   };
