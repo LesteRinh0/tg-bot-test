b3049b5c68f3cc3d4e7f8de32fb12d1d82d0f14c
LesteR <example@gmail.com>
2023-07-05 18:31:13 +0300

    fix: saveTask func

--- src/commands/saveTask.js
+++ src/commands/saveTask.js
@@ -3,49 +3,54 @@ import { collection } from '../configs/mongoConfig.js';
 import { setReminder } from '../helpers/setReminder.js';
 
 export async function createTask(createTaskMsg) {
-    const chatId = createTaskMsg.chat.id;
-  
-    await bot.sendMessage(chatId, 'Введите задачу:').then(() => {
-      const taskSaved = false;
-      bot.once('text', (msg) => {
-        if (!taskSaved) {
-          const task = msg.text;
-  
-          try {
-            collection.insertOne({
-              taskId: msg.message_id,
-              task,
-              id: chatId,
-              name: msg.chat.first_name,
-            });
-  
-            const opts = {
-              reply_markup: {
-                inline_keyboard: [
-                  [
-                    {
-                      text: 'Да',
-                      callback_data: `yes/${msg.message_id}`,
-                    },
-                    {
-                      text: 'Нет',
-                      callback_data: `no/${msg.message_id}`,
-                    },
-                  ],
-                ],
-              },
-            };
-  
-            bot.sendMessage(
-              msg.from.id,
-              'Хотите получить напоминание о задаче?',
-              opts
-            );
-            bot.on('callback_query', setReminder);
-          } catch (error) {
-            bot.sendMessage(chatId, 'Ошибка при сохранении задачи.');
-          }
+  const chatId = createTaskMsg.chat.id;
+
+  await bot.sendMessage(chatId, 'Введите задачу:').then(() => {
+    const taskSaved = false;
+    bot.once('text', (msg) => {
+      if (!taskSaved) {
+        const task = msg.text;
+
+        if (task.startsWith("/")) {
+          bot.sendMessage(chatId, 'Ошибка: задача не может начинаться с "/"');
+          return;
         }
-      });
+
+        try {
+          collection.insertOne({
+            taskId: msg.message_id,
+            task,
+            id: chatId,
+            name: msg.chat.first_name,
+          });
+
+          const opts = {
+            reply_markup: {
+              inline_keyboard: [
+                [
+                  {
+                    text: 'Да',
+                    callback_data: `yes/${msg.message_id}`,
+                  },
+                  {
+                    text: 'Нет',
+                    callback_data: `no/${msg.message_id}`,
+                  },
+                ],
+              ],
+            },
+          };
+
+          bot.sendMessage(
+            msg.from.id,
+            'Хотите получить напоминание о задаче?',
+            opts
+          );
+          bot.on('callback_query', setReminder);
+        } catch (error) {
+          bot.sendMessage(chatId, 'Ошибка при сохранении задачи.');
+        }
+      }
     });
-  };
+  });
+};
