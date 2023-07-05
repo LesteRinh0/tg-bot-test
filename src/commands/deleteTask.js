fd3beb13f2c5dcce7d8ec7e085f5fe465e7aa417
LesteR <example@gmail.com>
2023-07-05 18:18:50 +0300

    fix deleteTask func

--- src/commands/deleteTask.js
+++ src/commands/deleteTask.js
@@ -1,36 +1,32 @@
 import { bot } from "../configs/botConfig.js";
-import { sendErrorMessage } from "../helpers/sendErrorMessage.js";
 import { collection } from "../configs/mongoConfig.js";
 
 export async function deleteTask(msg) {
     const chatId = msg.chat.id;
-
+  
     const tasks = await collection.find({ id: chatId, task: { $exists: true } }).toArray();
-        let tasksMsg = '\n';;
-        if (tasks.length === 0) {
-            tasksMsg = 'У вас нету запланированных задач';
-            bot.sendMessage(chatId, tasksMsg);
-        } else {
-            tasks.forEach((task) => {
-                    tasksMsg += `• ${task.task}\n`;});
-          bot.sendMessage(chatId, 'Выберите задачу для удаления:' + tasksMsg);
-
-          bot.once('message', (msg) => {
-          const selectedTaskIndex = parseInt(msg.text) - 1;
-
-          if (isNaN(selectedTaskIndex) || selectedTaskIndex < 0 || selectedTaskIndex >= tasks.length) {
+    let tasksMsg = '\n';
+    if (tasks.length === 0) {
+      tasksMsg = 'У вас нету запланированных задач';
+      bot.sendMessage(chatId, tasksMsg);
+    } else {
+      tasks.forEach((task, index) => {
+        tasksMsg += `${index + 1}. ${task.task}\n`;
+      });
+      bot.sendMessage(chatId, 'Выберите задачу для удаления:' + tasksMsg);
+  
+      bot.once('message', async (msg) => {
+        const selectedTaskIndex = parseInt(msg.text) - 1;
+  
+        if (isNaN(selectedTaskIndex) || selectedTaskIndex < 0 || selectedTaskIndex >= tasks.length) {
           bot.sendMessage(chatId, 'Некорректный номер задачи. Попробуйте еще раз');
-          }
-
-          const selectedTask = tasks[selectedTaskIndex];
-
-          collection.deleteOne({ _id: selectedTask._id }, (err, result) => {
-          if (err) {
-            sendErrorMessage(chatId, bot);
-          }
-
-          bot.sendMessage(chatId, `Задача "${selectedTask.name}" успешно удалена`);
-            });
-          });
-        }            
-  };
+          return;
+        }
+  
+        const selectedTask = tasks[selectedTaskIndex];
+  
+        await collection.deleteOne({ _id: selectedTask._id });
+        bot.sendMessage(chatId, `Задача "${selectedTask.task}" успешно удалена`);
+      });
+    }
+  }
