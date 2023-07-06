770e6004adeedad565f32c69dbde389ba9f113ff
LesteR <example@gmail.com>
2023-07-06 16:10:11 +0300

    feat imports split

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 import schedule from 'node-schedule';
 
-import { keys } from './src/constants/constants.js';
-import { client, collection } from './src/configs/mongoConfig.js';
-import { bot } from './src/configs/botConfig.js';
-import { processCommand } from './src/commands/mainFunctions.js';
-import { createTask } from './src/commands/saveTask.js';
-import { app } from './src/configs/serverConfig.js';
-import { viewTasks } from './src/commands/tasks.js';
-import { sendWeather } from './src/commands/sendWeather.js';
-import { recommendCity } from './src/commands/recommendCity.js';
-import { handleSubscribe } from './src/commands/handleSubscribe.js';
-import { handleUnsubscribe } from './src/commands/handleUnsubscribe.js';
-import { gracefulShutdown } from './src/configs/gracefulShutdown.js';
-import { deleteTask } from './src/commands/deleteTask.js';
-
+import '.imports/imports.js';
 
 process.on('unhandledRejection', (error) => {
   console.error('Unhandled Rejection:', error);
