61612d4036feb55cd48da4a341deac324af4a078
LesteR <example@gmail.com>
2023-07-06 15:37:46 +0300

    fix everyDayNotify func

--- src/helpers/everyDayNotify.js
+++ src/helpers/everyDayNotify.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 import "dotenv/config";
 
+import { links } from "../constants/constants";
+
 export const everyDayNotify = (bot, cityName, chatId) => async () => {
   const response = await axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.WEATHER_API}&units=metric`
+    `${links.weatherAPI}?q=${cityName}&appid=${process.env.WEATHER_API}&units=metric`
   );
   const temperature = response.data.main.temp;
   const description = response.data.weather[0].description;
