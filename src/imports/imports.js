cc9e161850d915e603e930e4bcf4d122872271a8
LesteR <example@gmail.com>
2023-07-06 17:40:17 +0300

    fix imports split

--- src/imports/imports.js
+++ src/imports/imports.js
@@ -1,17 +1,17 @@
 import schedule from 'node-schedule';
-import { keys } from './src/constants/constants.js';
-import { client, collection } from './src/configs/mongoConfig.js';
-import { bot } from './src/configs/botConfig.js';
-import { processCommand } from './src/commands/mainFunctions.js';
-import { createTask } from './src/commands/saveTask.js';
-import { app } from './src/configs/serverConfig.js';
-import { viewTasks } from './src/commands/tasks.js';
-import { sendWeather } from './src/commands/sendWeather.js';
-import { recommendCity } from './src/commands/recommendCity.js';
-import { handleSubscribe } from './src/commands/handleSubscribe.js';
-import { handleUnsubscribe } from './src/commands/handleUnsubscribe.js';
-import { gracefulShutdown } from './src/configs/gracefulShutdown.js';
-import { deleteTask } from './src/commands/deleteTask.js';
+import { keys } from '../constants/constants.js';
+import { client, collection } from '../configs/mongoConfig.js';
+import { bot } from '../configs/botConfig.js';
+import { processCommand } from '../commands/mainFunctions.js';
+import { createTask } from '../commands/saveTask.js';
+import { app } from '../configs/serverConfig.js';
+import { viewTasks } from '../commands/tasks.js';
+import { sendWeather } from '../commands/sendWeather.js';
+import { recommendCity } from '../commands/recommendCity.js';
+import { handleSubscribe } from '../commands/handleSubscribe.js';
+import { handleUnsubscribe } from '../commands/handleUnsubscribe.js';
+import { gracefulShutdown } from '../configs/gracefulShutdown.js';
+import { deleteTask } from '../commands/deleteTask.js';
 
 export {
   schedule,
